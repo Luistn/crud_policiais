16d56b30dad20f511bf44ff5e228244be913c7b7
luistn <luiis.torres@hotmail.com>
2025-08-25 11:08:03 -0300

    criação front

---
+++ frontend/src/app/cadastro-policiais.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-policiais',
+  imports: [],
+  templateUrl: './cadastro-policiais.component.html',
+  styleUrl: './cadastro-policiais.component.css'
+})
+export class CadastroPoliciaisComponent {
+
+}
